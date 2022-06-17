3874f1f7c51f77d0cd677ef187865c6e256e1274
anupamapan9 <anupamapan0905@gmail.com>
2022-06-17 09:59:55 +0600

    pop up added

--- src/components/Countries/CountryCard.js
+++ src/components/Countries/CountryCard.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CountryDetailPopUp from './CountryDetailPopUp';
 
 const CountryCard = ({ country }) => {
     const { flags, name } = country;
-    console.log(country)
+    const [confirm, setConfirm] = useState(false)
     return (
-        <div class="card bg-base-100 shadow-xl image-full">
-            <figure><img src={flags?.png} className="w-full" alt="Shoes" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">{name.common}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Details</button>
+        <>
+            <div class="card bg-base-100 shadow-xl image-full">
+                <figure><img src={flags?.png} className="w-full" alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{name.common}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <label class="btn btn-primary" onClick={() => setConfirm(!confirm)} for="my-modal-3" >Details</label>
+                    </div>
                 </div>
+                {
+                    confirm && <CountryDetailPopUp setConfirm={setConfirm} confirm={confirm} />
+                }
             </div>
-        </div>
+
+
+        </>
+
     );
 };
 
---
+++ src/components/Countries/CountryDetailPopUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CountryDetailPopUp = ({ setConfirm, confirm }) => {
+    return (
+        <div>
+            <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+            <div class="modal">
+                <div class="modal-box relative">
+                    <label for="my-modal-3" onClick={() => setConfirm(!confirm)} class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
+                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CountryDetailPopUp;
