8b63475ba0be62781ace5e5ab7e29b0550125c66
anupamapan9 <anupamapan0905@gmail.com>
2022-06-17 09:45:31 +0600

    added card

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -3,19 +3,36 @@ import CountryCard from './CountryCard';
 
 const Conutries = () => {
     const [countries, setCountries] = useState([])
+    const [url, setUrl] = useState("https://restcountries.com/v3.1/all")
+    console.log(countries)
     useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all")
+        fetch(url)
             .then(res => res.json())
-            .then(data => setCountries(data))
-    }, [])
+            .then(data => {
+                if (data.status === 404) {
+                    setCountries(null)
+                    return
+                }
+                if (data.message === "Page Not Found") {
+                    setCountries(null)
+                    return
+                }
+                setCountries(data)
+            })
+    }, [url])
+
     const search = e => {
         e.preventDefault()
         const text = e.target.text.value;
-        console.log(text)
+        if (text === '') {
+            return
+        }
+        setUrl(`https://restcountries.com/v3.1/name/${text}`)
     }
+
     return (
         <section>
-            <div className='w-full m-auto'>
+            <div className='flex justify-center py-10'>
                 <form onSubmit={search} className="form-control">
                     <div className="input-group">
                         <input type="text" placeholder="Search…" name='text' className="input input-bordered" />
@@ -23,9 +40,18 @@ const Conutries = () => {
                     </div>
                 </form>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:gap-10 px-10'>
-                {countries?.map(country => <CountryCard country={country} key={country.ccn3} />)}
-            </div>
+            {
+                countries === null && <div>
+                    <h1 className='text-center font-bold text-4xl text-red-600'>No result found</h1>
+                </div>
+            }
+            {
+                countries && <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 lg:gap-10 px-10'>
+                    {
+                        countries?.map(country => <CountryCard country={country} key={country.ccn3} />)}
+                </div>
+            }
+
         </section>
     );
 };
--- src/components/Countries/CountryCard.js
+++ src/components/Countries/CountryCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const CountryCard = ({ country }) => {
-    const { flags, } = country;
+    const { flags, name } = country;
     console.log(country)
     return (
         <div class="card bg-base-100 shadow-xl image-full">
             <figure><img src={flags?.png} className="w-full" alt="Shoes" /></figure>
             <div class="card-body">
-                <h2 class="card-title">Shoes!</h2>
+                <h2 class="card-title">{name.common}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div class="card-actions justify-end">
                     <button class="btn btn-primary">Details</button>
